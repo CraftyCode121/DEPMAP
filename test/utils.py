c92d3a8f2d53abd41c280d16a7d5ffb1618b042b
Hassan <craftycode121@gmail.com>
2026-08-21 12:01:23 +0500

    first commit

---
+++ test/utils.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Dog:
+    def bark(self):
+        return "Woof woof"
+        
+class Cat:
+    def cry(self):
+        return "Meaw mraw"
+        
+X = np.array([1, 2, 3])
+y = np.array([2, 4, 6])
